c2e3c7d07ea817087aae691dfdc670e4996c249f
Guillem <guillembaracco@hotmail.com>
2022-09-09 09:22:01 +0200

    before merge night work with user profile view

--- src/views/ResultsAndStadistics.jsx
+++ src/views/ResultsAndStadistics.jsx
@@ -1,6 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import { useParams, useNavigate } from 'react-router-dom';
-import axios from 'axios';
+import React from 'react';
+
 
 export default function ResultsAndStadistic() {
 
