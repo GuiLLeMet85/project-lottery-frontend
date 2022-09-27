a999fc63df3ad3b40f40c9f35d8002f7dea4d07f
Guillem B <guillembaracco@gmail.com>
2022-09-27 17:52:04 +0200

    add error 404 page

--- src/views/ErrorPage.jsx
+++ src/views/ErrorPage.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import React from "react";
 
 export default function ErrorPage() {
-  return (
-    <div>404 - Page not found</div>
-  )
+
+    return (
+        <div className="sect-error-404">
+            <h1>404 - Page not found</h1>
+             <img src= "https://media.giphy.com/media/VbnUQpnihPSIgIXuZv/giphy.gif" alt="cat-finding" />
+        </div>
+
+    )
 }
